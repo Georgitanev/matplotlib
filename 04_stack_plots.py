c41c19f6ce5c1a886fb7f3107275d70a8a38262f
Georgitanev <georgi.tanev86@gmail.com>
2020-03-18 21:40:43 +0200

    Stack plots

---
+++ 04_stack_plots.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 21:37:08 2020
+
+@author: G
+https://matplotlib.org/gallery/lines_bars_and_markers/stackplot_demo.html
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y1 = [1, 1, 2, 3, 5]
+y2 = [0, 4, 2, 6, 8]
+y3 = [1, 3, 5, 7, 9]
+
+y = np.vstack([y1, y2, y3])
+
+
+labels = ["Fibonacci ", "Evens", "Odds"]
+
+fig, ax = plt.subplots()
+ax.stackplot(x, y1, y2, y3, labels=labels)
+ax.legend(loc='upper left')
+plt.show()
+
+fig, ax = plt.subplots()
+ax.stackplot(x, y)
+plt.show()
