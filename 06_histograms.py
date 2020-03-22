c60367fb6cedafcc8fa4657f4e54e0c5530cce70
Georgitanev <georgi.tanev86@gmail.com>
2020-03-22 23:38:09 +0200

    histograms

---
+++ 06_histograms.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
+
+plt.hist(ages)
+data = pd.read_csv('data_histograms.csv')
+
+ids = data['Responder_id']
+ages = data['Age']
+
+bins = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+plt.hist(ages, bins=bins, edgecolor='black', log=True)
+
+median_age = 29
+color = 'blue'
+
+plt.axvline(median_age, color=color, label='Age Median', linewidth=2)
+
+plt.legend()
+
+plt.title('Ages of Respondents')
+plt.xlabel('Ages')
+plt.ylabel('Total Respondents')
+
+plt.tight_layout()
+
+plt.show()
